0389a667ffa778134e0762910ca758eecf116a13
Sam Hokin <shokin@ncgr.org>
2020-04-07 18:04:28 -0600

    4/7 update.

--- places.R
+++ places.R
@@ -1,2 +1,2 @@
 ## list our places of interest in one file
-places = c("Italy", "Spain", "US", "France", "UK", "Iran", "China", "Netherlands", "Germany", "Sweden", "Canada", "SKorea", "Denmark", "Poland", "Israel", "Russia", "Australia")
+places = c("Italy", "Spain", "US", "France", "UK", "Iran", "China", "Netherlands", "Germany", "Sweden", "Canada", "Denmark", "SKorea", "Poland", "Israel", "Russia", "Australia")
