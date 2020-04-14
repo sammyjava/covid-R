daf6ac6cbe5aa3c08dde29d827f11c4a240376b8
Sam Hokin <shokin@ncgr.org>
2020-04-13 19:22:39 -0600

    4/13 update.

--- places.R
+++ places.R
@@ -1,2 +1,2 @@
 ## list our places of interest in one file
-places = c("US", "Italy", "Spain", "France", "UK", "Iran", "China", "Germany", "Netherlands", "Sweden", "Canada", "Denmark", "SKorea", "Poland", "Israel", "Russia", "Australia")
+places = c("US", "Italy", "Spain", "France", "UK", "Iran", "China", "Germany", "Netherlands", "Sweden", "Canada", "Denmark", "Poland", "SKorea", "Russia", "Israel", "Australia")
--- x100.R
+++ x100.R
@@ -1,4 +1,4 @@
 ## day at which each country passed 100 deaths
-places = c("US", "Italy", "Spain", "France", "UK")
-x100 = c(56, 43, 51, 54, 58)
+places = c("US", "Italy", "Spain", "France", "UK", "China", "SKorea", "Germany")
+x100 = c(56, 43, 51, 54, 58, 7, 60, 61)
 
