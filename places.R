dbef3bc40a6a1490ef12ac1375deb0f1bbd4272f
Sam Hokin <shokin@ncgr.org>
2020-04-26 18:57:30 -0600

    4/26 update.

--- places.R
+++ places.R
@@ -15,6 +15,6 @@
 ## 206                        Sweden     1203
 ## 207                   Switzerland     1239
 
-places = c("US", "Italy", "Spain", "France", "UK", "Belgium", "Iran", "Germany", "China", "Netherlands", "Brazil", "Turkey", "Sweden", "Switzerland")
-x100   = c(  56,      43,      51,       54,   57,        62,     44,       61,        7,            59,       67,       67,       68,            62)
-popmil = c(328.2,   60.3,    47.0,     67.1, 67.9,      11.5,   83.2,     83.1,   1427.6,          17.4,    210.1,     83.2,     10.3,          8.57)
+places = c("US", "Italy", "Spain", "France", "UK", "Belgium", "Germany", "Iran", "China", "Netherlands", "Brazil", "Turkey", "Sweden", "Switzerland")
+x100   = c(  56,      43,      51,       54,   57,        62,        61,     44,        7,            59,       67,       67,       68,            62)
+popmil = c(328.2,   60.3,    47.0,     67.1, 67.9,      11.5,      83.1,   83.2,   1427.6,          17.4,    210.1,     83.2,     10.3,          8.57)
